92600912554513d33bdd7130418922989128d77a
Daria Dotsenko <dotsenko8268@gmail.com>
2023-03-02 21:55:22 +0200

    2. useRef()

--- src/app/components/examples/useRef/prevState.jsx
+++ src/app/components/examples/useRef/prevState.jsx
@@ -1,10 +1,24 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import CardWrapper from "../../common/Card";
 import SmallTitle from "../../common/typografy/smallTitle";
+import Divider from "../../common/divider";
 const PrevStateExample = () => {
+    const prevState = useRef("");
+    const [otherState, setOtherState] = useState("false");
+    console.log(otherState);
+    const toggleOtherState = () => {
+        setOtherState(prevState => prevState === "false" ? "true" : "false");
+    };
+    useEffect(() => {
+        prevState.current = otherState;
+    }, [otherState]);
     return (
         <CardWrapper>
             <SmallTitle>Предыдущее состояние</SmallTitle>
+            <Divider />
+            <p>prev State: {prevState.current}</p>
+            <p>current State: {otherState}</p>
+            <button className='btn btn-primary' onClick={toggleOtherState}>Toggle other state</button>
         </CardWrapper>
     );
 };
